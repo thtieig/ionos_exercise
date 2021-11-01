d8fe5e0c3d21141d1f459547bc9c2c87e73f6119
ChrisTian <10831582+thtieig@users.noreply.github.com>
2021-11-01 18:55:35 +0100

    Terraform creation dc ip cluster - missing pool

--- k8s/k8scluster-vars.tf
+++ k8s/k8scluster-vars.tf
@@ -2,7 +2,18 @@ variable "ionoscloud_username" {
   type = string
   description = "IONOS username"
 }
+
 variable "ionoscloud_password" {
   type = string
   description = "IONOS password"
+}
+
+variable "dcid" {
+  type = string
+  description = "Datacenter UUID from IONOS Control Panel"
+}
+
+variable "ionoscloud_datacenter_location" {
+  type = string
+  description = "IONOS The regional location for this IP Block: us/las, us/ewr, de/fra, de/fkb"
 }
